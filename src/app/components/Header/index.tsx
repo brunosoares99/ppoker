52252783713762b1e30366d1352b21a6a134225b
brunosoares99 <bruno_soares_vieira@outlook.com>
2023-07-16 00:09:30 -0300

    feat: implements home page at project

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="p-4 flex justify-between items-center w-full text-gray-900">
+      <div className="text-3xl font-bold">
+        Scrum Poker
+      </div>
+      <button className="bg-blue-800 px-4 text-white py-2 rounded-lg font-medium " >Juntar-se</button>
+    </header>
+  )
+}
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,16 @@
-export default function Home() {
-  return <h1>Hi</h1>
+const HomePage = ()=> {
+  return (
+    <main className="w-full h-full">
+      <section className="w-full h-full flex flex-col items-center justify-center">
+        <div className="flex items-center flex-col -translate-y-2/4">
+          <div className="text-center mb-4">
+            <h1 className="text-3xl font-bold text-gray-900">Scrum Poker</h1>
+            <h2 className="text-gray-500">para times de desenvolvimento ágil</h2>
+          </div>
+          <button className="rounded-lg font-semibold text-gray-50 px-14 py-2 bg-blue-800">Criar novo jogo</button>
+        </div>
+      </section>
+    </main>
+  )
 }
-
+export default HomePage;
